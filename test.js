e7dc9d5789bfbd2e9f8742140bc6686d423526c7
lLeahDizon <516330797@qq.com>
2021-09-13 20:22:34 +0800

    生成一条数据到数据库

--- test.js
+++ test.js
@@ -1,17 +1,24 @@
-const { Sequelize, Model, DataTypes } = require('sequelize');
-const sequelize = new Sequelize('sqlite::memory:');
+const {Sequelize, Model, DataTypes} = require('sequelize')
+const sequelize = new Sequelize('lemon', 'root', '123456', {
+  dialect: 'mysql'
+})
 
+// 创建 User 模型
 class User extends Model {}
+// 初始化 User
 User.init({
   username: DataTypes.STRING,
   birthday: DataTypes.DATE
-}, { sequelize, modelName: 'user' });
+}, {sequelize, modelName: 'user'});
 
 (async () => {
-  await sequelize.sync();
+  // 同步到数据库
+  await sequelize.sync()
+  // 创建一条记录
   const jane = await User.create({
     username: 'janedoe',
     birthday: new Date(1980, 6, 20)
-  });
-  console.log(jane.toJSON());
-})();
+  })
+  // 打印结果
+  console.log(jane.toJSON())
+})()
