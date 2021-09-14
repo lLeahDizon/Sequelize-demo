4e2b93554b2cbc3762ccd6c2bd3ef21b4f0d3a51
lLeahDizon <516330797@qq.com>
2021-09-14 23:46:17 +0800

    调用 sequelize API

--- test.js
+++ test.js
@@ -5,20 +5,34 @@ const sequelize = new Sequelize('lemon', 'root', '123456', {
 
 // 创建 User 模型
 class User extends Model {}
+
 // 初始化 User
 User.init({
   username: DataTypes.STRING,
   birthday: DataTypes.DATE
-}, {sequelize, modelName: 'user'});
+}, {sequelize, modelName: 'user'})
 
-(async () => {
-  // 同步到数据库
-  await sequelize.sync()
-  // 创建一条记录
-  const jane = await User.create({
-    username: 'janedoe',
-    birthday: new Date(1980, 6, 20)
+async function run() {
+  User.destroy({
+    where: {
+      id: 1
+    }
   })
-  // 打印结果
-  console.log(jane.toJSON())
-})()
+  const users = await User.findAll()
+  console.log(JSON.stringify(users))
+  sequelize.close()
+}
+
+run()
+
+// (async () => {
+//   // 同步到数据库
+//   await sequelize.sync()
+//   // 创建一条记录
+//   const jane = await User.create({
+//     username: 'janedoe',
+//     birthday: new Date(1980, 6, 20)
+//   })
+//   // 打印结果
+//   console.log(jane.toJSON())
+// })()
